c47cfdf8f2110d48d3245cf037a45fb294acec98
Miguel Celedon <miguelceledon@outlook.com>
2015-09-03 22:47:06 -0430

    Commented insecure line

--- platform-visualization/common.php
+++ platform-visualization/common.php
@@ -8,7 +8,8 @@ function askGitHub($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    //Uncomment below if testing locally
+    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'User-Agent: MALOTeam',
         //'User-Agent: Miguelcldn', //For testing
