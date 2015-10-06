68a3ab3cc0888ee8144ca53389109185711f9506
Luis Fuenmayor <fuelusumar@gmail.com>
2015-10-06 14:12:06 -0430

    test on server

--- api-server/app.js
+++ api-server/app.js
@@ -37,9 +37,13 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost, http://127.0.0.1, *');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.header("Access-Control-Allow-Origin", "*");
+    //res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     next();
 });
 
