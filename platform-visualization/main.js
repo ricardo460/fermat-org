e177e8f4fcba57f148eca39041b463bd31cb99a9
Luis Fuenmayor <fuelusumar@gmail.com>
2015-10-06 13:53:56 -0430

    fix on getData()

--- platform-visualization/main.js
+++ platform-visualization/main.js
@@ -283,8 +283,8 @@ var superLayers = {
 };
 
 var viewManager = new ViewManager();
-//var URL = "get_plugins.php";
-var URL = "http://52.11.156.16:3000/repo/comps";
+var URL = "get_plugins.php";
+//var URL = "http://52.11.156.16:3000/repo/comps";
 
 function getData() {
     $.ajax({
@@ -293,14 +293,14 @@ function getData() {
     }).success(
         function(lists) {
             console.dir(lists);
-            /*var l = JSON.parse(lists);
+            var l = JSON.parse(lists);
             viewManager.fillTable(l);
             $('#splash').fadeTo(2000, 0, function() {
                 $('#splash').remove();
                 init();
                 //setTimeout(animate, 500);
                 animate();
-            });*/
+            });
         }
     );
 
