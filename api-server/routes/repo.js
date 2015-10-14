67ae771c1c686ee45c8408bb37e2a97956c3d192
Luis Fuenmayor <fuelusumar@gmail.com>
2015-10-14 12:56:56 -0430

    new jslint config repo router lint ok

--- api-server/routes/repo.js
+++ api-server/routes/repo.js
@@ -1,9 +1,6 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
-var apicache = require('apicache')
-    .options({
-        debug: true
-    }).middleware;
 var repMod = require('../modules/repository');
 var Cache = require('../lib/route-cache');
 
@@ -13,41 +10,17 @@ var Cache = require('../lib/route-cache');
  * @route
  *
  */
-router.put('/comps', function(req, res, next) {
-    repMod.updComps(req, function(error, result) {
-        if (error) res.status(200).send(error);
-        else res.status(200).send(result);
-    });
-});
-
-/**
- * [description]
- *
- * @route
- *
- */
-router.get('/comps', function(req, res, next) {
-    // creation of object cache
-    var cache = new Cache({
-        type: 'memory',
-        time: 36000000
-    }, req);
-    // we search for body in cache
-    var body = cache.getBody();
-    if (body) {
-        // we send it
-        res.status(200).send(body);
-    } else {
-        // we create it
-        repMod.getComps(req, function(error, result) {
+router.put('/comps', function (req, res, next) {
+    try {
+        repMod.updComps(req, function (error, result) {
             if (error) {
                 res.status(200).send(error);
             } else {
-                // we save it
-                cache.setBody(result);
                 res.status(200).send(result);
             }
         });
+    } catch (err) {
+        next(err);
     }
 });
 
@@ -57,11 +30,33 @@ router.get('/comps', function(req, res, next) {
  * @route
  *
  */
-router.post('/comps', function(req, res, next) {
-    repMod.loadComps(req, function(error, result) {
-        if (error) res.status(200).send(error);
-        else res.status(200).send(result);
-    });
+router.get('/comps', function (req, res, next) {
+    try {
+        // creation of object cache
+        var cache = new Cache({
+            type: 'memory',
+            time: 36000000
+        }, req);
+        // we search for body in cache
+        var body = cache.getBody();
+        if (body) {
+            // we send it
+            res.status(200).send(body);
+        } else {
+            // we create it
+            repMod.getComps(req, function (error, result) {
+                if (error) {
+                    res.status(200).send(error);
+                } else {
+                    // we save it
+                    cache.setBody(result);
+                    res.status(200).send(result);
+                }
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
 });
 
 /**
@@ -70,18 +65,68 @@ router.post('/comps', function(req, res, next) {
  * @route
  *
  */
-router.post('/devs', function(req, res, next) {
-    repMod.updDevs(req, function(error, result) {
-        if (error) res.status(200).send(error);
-        else res.status(200).send(result);
-    });
+router.post('/comps', function (req, res, next) {
+    try {
+        repMod.loadComps(req, function (error, result) {
+            if (error) {
+                res.status(200).send(error);
+            } else {
+                res.status(200).send(result);
+            }
+        });
+    } catch (err) {
+        next(err);
+    }
 });
 
-router.get('/procs', apicache('5 minutes'), function(req, res, next) {
-    repMod.getProcs(req, function(error, result) {
-        if (error) res.status(200).send(error);
-        else res.status(200).send(result);
-    });
+/**
+ * [description]
+ *
+ * @route
+ *
+ */
+router.post('/devs', function (req, res, next) {
+    try {
+        repMod.updDevs(req, function (error, result) {
+            if (error) {
+                res.status(200).send(error);
+            } else {
+                res.status(200).send(result);
+            }
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/procs', function (req, res, next) {
+    try {
+        // creation of object cache
+        var cache = new Cache({
+            type: 'memory',
+            time: 36000000
+        }, req);
+        // we search for body in cache
+        var body = cache.getBody();
+        if (body) {
+            // we send it
+            res.status(200).send(body);
+        } else {
+            // we create it
+            repMod.getProcs(req, function (error, result) {
+                if (error) {
+                    res.status(200).send(error);
+                } else {
+                    // we save it
+                    cache.setBody(result);
+                    res.status(200).send(result);
+                }
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
+
 });
 
 module.exports = router;
