8ffbd9911a1d20845efd46fef8c3e1f651d010c2
Miguel Celedon <miguelceledon@outlook.com>
2015-10-21 13:44:15 -0430

    Fix #154 Transparent logo and preloading in same page

--- platform-visualization/src/getData.js
+++ platform-visualization/src/getData.js
@@ -4,17 +4,24 @@ var tileManager = new TileManager();
 
 function getData() {
     animate();
+    
     $.ajax({
         url: "http://52.11.156.16:3000/repo/comps?access_token=561fd1a5032e0c5f7e20387d",
         method: "GET"
     }).success(
         function (lists) {
-            tileManager.fillTable(lists);
-            window.browserManager.createButton();
-            TWEEN.removeAll();
-            logo.stopFade();
-            init();
+            
+            window.preLoad(function() {
+                
+                tileManager.fillTable(lists);
+                window.browserManager.createButton();
+                TWEEN.removeAll();
+                logo.stopFade();
+                init();
+            });
         });
+    
+    
 /*setTimeout(function(){
         var l = JSON.parse(testData);
 
