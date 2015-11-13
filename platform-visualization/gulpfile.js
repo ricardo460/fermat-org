1afb0a74f1ea35c2bd8d52c27d353fa37faf1f30
Miguel Celedon <miguelceledon@outlook.com>
2015-11-12 20:17:13 -0430

    Enforced JSHint for compilation

--- platform-visualization/gulpfile.js
+++ platform-visualization/gulpfile.js
@@ -2,6 +2,9 @@
 var gulp = require('gulp');
 // Include plugins
 var concat = require('gulp-concat');
+var map = require('map-stream');
+var jshint = require('gulp-jshint')
+
 // Concatenate JS Files
 gulp.task('scripts', function() {
     return gulp.src('src/*.js')
@@ -9,9 +12,23 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('.'));
 });
 
+//Fail if jsHint fails
+var exitOnJshintError = map(function (file, cb) {
+  if (!file.jshint.success) {
+    console.error('jshint failed');
+    process.exit(1);
+  }
+});
+gulp.task('lint', function() {
+  gulp.src('main.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(exitOnJshintError);
+});
+
 // Default task
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'lint', 'watch']);
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.js', ['scripts']);
+    gulp.watch('src/*.js', ['scripts', 'lint']);
 });
--- platform-visualization/src/viewer.js
+++ platform-visualization/src/viewer.js
@@ -187,10 +187,6 @@ function goToView ( targetView ) {
             getHeaderFLowAndPosition();
 
             break;
-            
-            bookManager.createBook(targetView); 
-            
-            break;
         default:
             
             if(window.map.views[targetView] == null)
@@ -649,7 +645,7 @@ function getHeaderFLowAndPosition() {
                             positionX = positionX - 1000;
                     }
                     //calculando X
-                    for(x = 0; x < column; x++){  //columnas              
+                    for(x = 0; x < column; x++){  //columnas
 
                         position = new THREE.Vector3();
 
