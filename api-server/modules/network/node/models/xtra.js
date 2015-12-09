bfad4dafe493a7df3cbf6ba70d2e86bb9012f839
Luis Fuenmayor <fuelusumar@gmail.com>
2015-12-09 16:07:51 -0300

    some fixes

--- api-server/modules/network/node/models/xtra.js
+++ api-server/modules/network/node/models/xtra.js
@@ -13,12 +13,24 @@
 function XtraMdl(os, sub, curncy, symbl, balnc, status) {
     'use strict';
     // always initialize all instance properties
-    if (os) this.os = os;
-    if (sub) this.sub = sub;
-    if (curncy) this.curncy = curncy;
-    if (symbl) this.symbl = symbl;
-    if (balnc) this.balnc = balnc;
-    if (status) this.status = status;
+    if (os) {
+        this.os = os.toLowerCase();
+    }
+    if (sub) {
+        this.sub = sub.toLowerCase();
+    }
+    if (curncy) {
+        this.curncy = curncy.toLowerCase();
+    }
+    if (symbl) {
+        this.symbl = symbl.toUpperCase();
+    }
+    if (balnc) {
+        this.balnc = balnc;
+    }
+    if (status) {
+        this.status = status;
+    }
 }
 
 // export the class
