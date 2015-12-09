a85ec8fc54679e308f0dc9f29906c0997b94f8b1
Miguel Celedon <miguelceledon@outlook.com>
2015-12-08 20:00:52 -0430

    Change network viewer to be inherited

--- platform-visualization/src/clientsViewer.js
+++ platform-visualization/src/clientsViewer.js
@@ -4,5 +4,52 @@ function ClientsViewer(parentNode) {
     var nodes = {};
     var edges = [];
     
+    function init() {
+        
+        loadData();
+        
+        for(var nodeID in nodes) {
+            
+            
+        }
+    }
     
+    function loadData() {
+        
+        test_loadData();
+    }
+    
+    function test_loadData() {
+        
+        var NUM_CLIENTS = 10;
+        
+        for(var i = 0; i < NUM_CLIENTS; i++) {
+            
+            nodes[i.toString()] = {
+                id : i.toString(),
+                edges : [
+                    { 
+                        id : parentNode.userData.id
+                    }
+                ]
+            };
+        }
+    }
+    
+    function createNode() {
+    }
+    
+    function showNodes() {}
+    
+    function hideNodes() {}
+    
+    function createEdges() {
+        
+    }
+    
+    function showEdges() {}
+    
+    function hideEdges() {}
+    
+    init();
 }
