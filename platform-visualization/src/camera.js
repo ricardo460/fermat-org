7daf6a4a9298a6136db122a4d5ad09607f7b27c8
ricardo460 <ricardodelgado460@hotmail.com>
2015-12-09 21:20:33 -0430

    Added further improvements to the screenshots.

--- platform-visualization/src/camera.js
+++ platform-visualization/src/camera.js
@@ -164,8 +164,6 @@ function Camera(position, renderer, renderFunc) {
     };
 
     this.setFocusScreenshots = function( id, duration ) {
-        
-        TWEEN.removeAll();
 
         focus = parseInt(id); 
 
