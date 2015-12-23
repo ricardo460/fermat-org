974af045ff9ab26346b7f7ed0fda8fc7cd928da7
German Mendoza <german.mendoza.187@gmail.com>
2015-12-22 21:55:58 -0430

    add route for nodes

--- api-server/lib/db/seedNetwork.js
+++ api-server/lib/db/seedNetwork.js
@@ -7,7 +7,7 @@ var modLink = require('../../modules/network/link')
 var servLink = require('../../modules/network/link/services/link')
 var modNode = require('../../modules/network/node')
 var servNode = require('../../modules/network/node/services/nod')
-
+var waves = [];
 
 /* Clear db */
 servWave.delAllWaves(function(){
@@ -23,7 +23,7 @@ servWave.delAllWaves(function(){
 });
 
 var loadWaves = function(){
-	var waves = [];
+	
 	for(var i= 0; i<10; i++){
 		modWav.insertWave(faker.lorem.sentence(),function(err, result){
 			if(err){
