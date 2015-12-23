2d6507d168ec0d2e33b44164ce318ea12e73b691
German Mendoza <german.mendoza.187@gmail.com>
2015-12-23 13:37:21 -0430

    add data in seeds

--- api-server/lib/db/seedNetwork.js
+++ api-server/lib/db/seedNetwork.js
@@ -1,5 +1,6 @@
 var faker = require('faker');
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 var db = require('../../db');
 var modWav = require('../../modules/network/wave')
 var servWave = require('../../modules/network/wave/services/wave')
@@ -8,6 +9,8 @@ var servLink = require('../../modules/network/link/services/link')
 var modNode = require('../../modules/network/node')
 var servNode = require('../../modules/network/node/services/nod')
 var waves = [];
+var linuxTypeServer = "linux";
+var nodeTypeServer = "server"; 
 
 /* Clear db */
 servWave.delAllWaves(function(){
@@ -24,14 +27,19 @@ servWave.delAllWaves(function(){
 
 var loadWaves = function(){
 	
-	for(var i= 0; i<10; i++){
-		modWav.insertWave(faker.lorem.sentence(),function(err, result){
-			if(err){
-				console.log("Error insert Wave");
-			}
-			else{
-				waves.push(result);
-			}
-		});
-	}
+	
+	modWav.insertWave(faker.lorem.sentence(),function(err, res){
+		if(err){
+			console.log("Error insert Wave");
+		}
+		else{
+			console.log("insert waves");
+			console.log(res);
+			var hash = crypto.createHash('md5').update(faker.name.firstName()).digest('hex');
+
+			modNode.insertNod(res._id, hash, "server", "linux");
+
+		}
+	});
+	
 }
