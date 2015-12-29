4ea7829527ec396f5d86500c8e92edf02eafde59
Luis Fuenmayor <fuelusumar@gmail.com>
2015-12-29 15:08:38 -0300

    add delAll to platform mod #318

--- api-server/modules/repository/platform/index.js
+++ api-server/modules/repository/platform/index.js
@@ -93,3 +93,27 @@ exports.getPlatfrms = function (callback) {
     }
 
 };
+
+
+/**
+ * [delAllPlatfrms description]
+ *
+ * @method delAllPlatfrms
+ *
+ * @param  {Function}     callback [description]
+ *
+ * @return {[type]}       [description]
+ */
+exports.delAllPlatfrms = function (callback) {
+    'use strict';
+    try {
+        layerSrv.delAllPlatfrms(function (err, platfrms) {
+            if (err) {
+                return callback(err, null);
+            }
+            return callback(null, true);
+        });
+    } catch (err) {
+        return callback(err, null);
+    }
+};
