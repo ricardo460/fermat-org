071c16116fef2ab169ad2ca97647884787f3f811
Luis Fuenmayor <fuelusumar@gmail.com>
2016-01-19 17:15:51 -0300

    close #361

--- api-server/modules/repository/lock/index.js
+++ api-server/modules/repository/lock/index.js
@@ -17,15 +17,11 @@ var LOCK_TIME = 300000;
  */
 exports.insOrUpdLock = function (_usr_id, _item_id, item_type, priority, callback) {
 	try {
-		console.log('0');
 		lockSrv.findLockByUsrIdAndItemId(_usr_id, _item_id, function (err_lock, res_lock) {
-			console.log('1');
 			if (err_lock) {
-				console.log('2');
 				return callback(err_lock, null);
 			}
 			if (res_lock) {
-				console.log('3');
 				var set_obj = {};
 				if (item_type && item_type !== res_lock.item_type) {
 					set_obj.item_type = item_type;
@@ -36,42 +32,32 @@ exports.insOrUpdLock = function (_usr_id, _item_id, item_type, priority, callbac
 					res_lock.priority = priority;
 				}
 				lockSrv.updateLockById(res_lock._id, set_obj, function (err_upd, res_upd) {
-					console.log('4');
 					if (err_upd) {
 						return callback(err_upd, null);
 					}
 					return callback(null, res_lock);
 				});
 			} else {
-				console.log('5');
 				lockSrv.findLockByItemId(_item_id, function (err_itm, res_itm) {
-					console.log('6');
 					if (err_lock) {
-						console.log('7');
 						return callback(err_lock, null);
 					}
 					if (res_itm) {
 						if (!dateLib.isDiffGr(res_itm.upd_at, LOCK_TIME)) {
-							console.log('8');
 							return callback(new Error('item is locked'), null);
 						} else {
 							lockSrv.delLockById(res_itm._id, function (err_del, res_del) {
-								console.log('9');
 								if (err_del) {
-									console.log('10');
 									return callback(err_del, null);
 								} else if (_usr_id && _item_id && item_type) {
-									console.log('11');
 									var lock = new LockMdl(_usr_id, _item_id, item_type, priority || 9);
 									lockSrv.insertLock(lock, function (err_ins, res_ins) {
-										console.log('12');
 										if (err_ins) {
 											return callback(err_ins, null);
 										}
 										return callback(null, res_ins);
 									});
 								} else {
-									console.log('13');
 									return callback(new Error('no valid parameters'), null);
 								}
 							});
@@ -79,7 +65,6 @@ exports.insOrUpdLock = function (_usr_id, _item_id, item_type, priority, callbac
 					} else {
 						var lock = new LockMdl(_usr_id, _item_id, item_type, priority || 9);
 						lockSrv.insertLock(lock, function (err_ins, res_ins) {
-							console.log('15');
 							if (err_ins) {
 								return callback(err_ins, null);
 							}
