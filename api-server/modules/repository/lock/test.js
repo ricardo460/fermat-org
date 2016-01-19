c5299125734f3c85df4cb50e30810796f581551b
Luis Fuenmayor <fuelusumar@gmail.com>
2016-01-19 16:09:48 -0300

    adding test

--- api-server/modules/repository/lock/test.js
+++ api-server/modules/repository/lock/test.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var db = require('../../../db');
+var lockMod = require('./index');
 var objid = new mongoose.Types.ObjectId();
 var dateLib = require('./libs/date');
 var timestamp = objid.getTimestamp();
@@ -7,3 +9,10 @@ console.dir(objid);
 console.dir(timestamp);
 console.dir(date);
 console.dir(dateLib.getObjIdToMilis(objid));
+/*lockMod.insOrUpdLock(new mongoose.Types.ObjectId(), new mongoose.Types.ObjectId(), 'layer', 5, function (err_ins, res_ins) {
+	if (err_ins) {
+		console.dir(err_ins);
+	} else {
+		console.dir(res_ins);
+	}
+});*/
