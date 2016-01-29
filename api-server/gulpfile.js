a3c303eaff598e5405e64c21538248d27fb49b13
Luis Fuenmayor <fuelusumar@gmail.com>
2016-01-29 10:07:00 -0300

    gulp ready

--- api-server/gulpfile.js
+++ api-server/gulpfile.js
@@ -1,35 +1,32 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-gulp.task('lint', function () {
+//
+gulp.task('jslint', function () {
 	return gulp.src([
 		'bin/*',
-		'lib/**/*.js',
-		'modules/**/*.js',
-		'routes/**/*.js',
+		'lib/**/*.js*',
+		'modules/**/*.js*',
+		'routes/**/*.js*',
 		'!node_modules/**/*',
-		'*.js'
+		'*.js*'
 	]).pipe(jshint({
 		'undef': true,
 		'unused': false,
-		//todo: true,
-		//this: true,
 		'node': true,
 		'nomen': true,
-		//vars: true,
 		'plusplus': false,
-		//stupid: false,
-		'latedef': true,
-		//unparam: true
+		'latedef': true
 	})).pipe(jshint.reporter(stylish));
 });
+//
 gulp.task('watch', function () {
 	gulp.watch([
 		'bin/*',
-		'lib/**/*.js',
-		'modules/**/*.js',
-		'routes/**/*.js',
+		'lib/**/*.js*',
+		'modules/**/*.js*',
+		'routes/**/*.js*',
 		'!node_modules/**/*',
-		'*.js'
-	], ['lint']);
+		'*.js*'
+	], ['jslint']);
 });
