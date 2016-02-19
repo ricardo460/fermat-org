1f93d4b1c897fb280d87534610bdb84fd6d851b0
Luis Campo <campusprize@gmail.com>
2016-02-19 17:02:40 -0430

    #443 Generated documentation html

--- api-server/apidoc/api_project.js
+++ api-server/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-02-19T12:26:12.968Z",
+    "time": "2016-02-19T20:19:45.300Z",
     "url": "http://apidocjs.com",
     "version": "0.14.0"
   }
