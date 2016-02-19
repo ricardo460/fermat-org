91d0082f0b764d05e949970f3f15ec96b2dc9e57
Luis Fuenmayor <fuelusumar@gmail.com>
2016-02-19 11:03:18 -0300

    fix ports

--- api-server/proxy.js
+++ api-server/proxy.js
@@ -1,51 +1,51 @@
 'use strict';
 var winston = require('winston'),
-    events = require('events'),
-    eventEmitter = new events.EventEmitter(),
-    http = require('http'),
-    httpProxy = require('http-proxy');
+	events = require('events'),
+	eventEmitter = new events.EventEmitter(),
+	http = require('http'),
+	httpProxy = require('http-proxy');
 var proxy_api = {
-    host: '127.0.0.1',
-    port: 3001
+	host: '127.0.0.1',
+	port: 80
 };
 var prod_api = {
-    host: '127.0.0.1',
-    port: 3000
+	host: '127.0.0.1',
+	port: 8080
 };
 var dev_api = {
-    host: '127.0.0.1',
-    port: 3002
+	host: '127.0.0.1',
+	port: 8081
 };
 /**
  * creates proxy servers and starts http server to redirect requests according to version
  */
 var starProxyServer = function () {
-    winston.log('info', 'Starting proxy server...');
-    winston.log('info', 'Creating proxy server to %s:%s...', prod_api.host, prod_api.port);
-    var prod_proxy = new httpProxy.createProxyServer({
-        target: prod_api
-    });
-    winston.log('info', 'Creating proxy server to %s:%s...', dev_api.host, dev_api.port);
-    var dev_proxy = new httpProxy.createProxyServer({
-        target: dev_api
-    });
-    winston.log('info', 'Starting http server on port %s...', proxy_api.port);
-    http.createServer(function (req, res) {
-        if (req.url.indexOf('env=development') > -1) {
-            // redirect to dev api
-            winston.log('info', 'Redirecting to dev api...');
-            dev_proxy.proxyRequest(req, res);
-        } else {
-            // redirect to prod api
-            winston.log('info', 'Redirecting to prod api...');
-            prod_proxy.proxyRequest(req, res);
-        }
-    }).listen(proxy_api.port);
+	winston.log('info', 'Starting proxy server...');
+	winston.log('info', 'Creating proxy server to %s:%s...', prod_api.host, prod_api.port);
+	var prod_proxy = new httpProxy.createProxyServer({
+		target: prod_api
+	});
+	winston.log('info', 'Creating proxy server to %s:%s...', dev_api.host, dev_api.port);
+	var dev_proxy = new httpProxy.createProxyServer({
+		target: dev_api
+	});
+	winston.log('info', 'Starting http server on port %s...', proxy_api.port);
+	http.createServer(function (req, res) {
+		if (req.url.indexOf('env=development') > -1) {
+			// redirect to dev api
+			winston.log('info', 'Redirecting to dev api...');
+			dev_proxy.proxyRequest(req, res);
+		} else {
+			// redirect to prod api
+			winston.log('info', 'Redirecting to prod api...');
+			prod_proxy.proxyRequest(req, res);
+		}
+	}).listen(proxy_api.port);
 };
 // action to take when events are emitted
 eventEmitter.on('starProxyServer', starProxyServer);
 // events emision
 eventEmitter.emit('starProxyServer');
 process.on('uncaughtException', function (err) {
-    winston.log('error', 'Error starting the proxy', err);
+	winston.log('error', 'Error starting the proxy', err);
 });
