73fbe94f00db00b7b90c0522adca2b8efa65abd1
Miguel Celedon <miguelceledon@outlook.com>
2016-03-03 17:19:43 -0430

    Updated client_id

--- platform-visualization/src/session.js
+++ platform-visualization/src/session.js
@@ -53,7 +53,7 @@ function Session(){
 	 * Login with github and gets the authorization code
 	 */
 	this.getAuthCode = function(){                                                                        //CLientID: c25e3b3b1eb9aa35c773 - Web
-		window.location.href = 'https://github.com/login/oauth/authorize?client_id=c25e3b3b1eb9aa35c773'; //ClientID: f079f2a8fa65313179d5 - localhost
+		window.location.href = 'https://github.com/login/oauth/authorize?client_id=d00a7c7d4489139327e4'; //ClientID: f079f2a8fa65313179d5 - localhost
 	};
 
 	/**
