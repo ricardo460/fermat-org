86631aaa8fd9743c00fbd2fc00b003543f657d23
Luis Fuenmayor <fuelusumar@gmail.com>
2016-03-09 10:56:51 -0300

    build 1457531804932

--- api-server/routes/v1/repo/comp/index.js
+++ api-server/routes/v1/repo/comp/index.js
@@ -14,7 +14,7 @@ var security = require('../../../../lib/utils/security');
  *
  * @return {[type]} [description]
  */
-var lock = function (req, res, next) {
+var lock = function (req, next) {
 	console.log('doing lock...');
 	try {
 		console.dir(req.params);
@@ -26,16 +26,16 @@ var lock = function (req, res, next) {
 			console.dir(req.body);
 			repMod.doLock(req, function (error, result) {
 				if (error) {
-					res.status(200).send(error);
+					next(error, null);
 				} else {
-					next();
+					next(null, result);
 				}
 			});
 		} else {
-			next();
+			next(null, null);
 		}
 	} catch (err) {
-		next(err);
+		next(err, null);
 	}
 };
 /**
@@ -61,7 +61,7 @@ var release = function (req) {
 /**
  * using lock for comp routes
  */
-router.use(lock);
+//router.use(lock);
 /**
  * @api {post} /v1/repo/usrs/:usr_id/comps add components
  * @apiVersion 0.0.1
@@ -300,18 +300,23 @@ router.delete('/:comp_id/comp-devs/:comp_dev_id', function (req, res, next) {
 router.get('/:comp_id', function (req, res, next) {
 	'use strict';
 	try {
-		repMod.getComp(req, function (error, result) {
+		lock(req, function (error, result) {
 			if (error) {
 				res.status(200).send(error);
 			} else {
-				if (result) {
-					res.status(200).send(result);
-				} else {
-					res.status(404).send({
-						message: "NOT FOUND"
-					});
-				}
-				release(req);
+				repMod.getComp(req, function (error, result) {
+					if (error) {
+						res.status(200).send(error);
+					} else {
+						if (result) {
+							res.status(200).send(result);
+						} else {
+							res.status(404).send({
+								message: "NOT FOUND"
+							});
+						}
+					}
+				});
 			}
 		});
 	} catch (err) {
