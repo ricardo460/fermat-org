ea59592444cd5492cf5b767cc5494ce58b938288
Luis Fuenmayor <fuelusumar@gmail.com>
2016-03-16 17:14:00 -0300

    merged

--- api-server/public/api_project.js
+++ api-server/public/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-03-14T14:17:34.803Z",
+    "time": "2016-03-16T20:13:54.250Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
