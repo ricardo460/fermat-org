420eea91ea61160f916b6e9fcebc0569ecde4970
Luis Fuenmayor <fuelusumar@gmail.com>
2016-04-01 15:45:26 -0300

    build 1459536320287

--- api-server/build/public/api_project.js
+++ api-server/build/public/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-04-01T18:44:47.837Z",
+    "time": "2016-04-01T18:45:21.356Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
