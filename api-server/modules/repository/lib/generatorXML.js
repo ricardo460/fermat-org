91dd6015342e916428f88e325daadda68ffb4c46
German Mendoza <german.mendoza.187@gmail.com>
2016-04-13 09:00:05 -0430

    add tokens and env to script

--- api-server/modules/repository/lib/generatorXML.js
+++ api-server/modules/repository/lib/generatorXML.js
@@ -1,12 +1,26 @@
 var fs = require('fs');
 var request = require('request');
 var js2xmlparser = require("js2xmlparser");
-var host = "http://localhost:8000/v1/"
-var axs_token = ""
-var usr_id = "qweqweqwe32132"
+var host = "http://api.fermat.org/v1/";
+var token = "";
+var usr_id = "";
+var env = "";
+if (process.env.NODE_ENV == "development") {
+    env = "development";
+    token = "56c7895cc01a204369238c4b";
+    usr_id = "56c7895cc01a204369238c43";
+} else if (process.env.NODE_ENV == "production") {
+    env = "production";
+    token = "56c621557d20701f4148eaad";
+    usr_id = "56c621557d20701f4148eaaa";
+} else {
+    env = "testing";
+    token = "56d9946df87ede9a50462120";
+    usr_id = "56d9946df87ede9a50462119";
+}
 
 //get superlayers
-request(host + "repo/usrs/"+usr_id+"/suprlays", function (error, response, body) {
+request(host + "repo/usrs/"+usr_id+"/suprlays?axs_key="+token+"&env="+env, function (error, response, body) {
   if (error) {
     console.log(error)
   } else {
@@ -17,7 +31,7 @@ request(host + "repo/usrs/"+usr_id+"/suprlays", function (error, response, body)
             return console.log(err);
         }
         //get layers
-        request(host + "repo/usrs/"+usr_id+"/layers", function (error, response, body) {
+        request(host + "repo/usrs/"+usr_id+"/layers?axs_key="+token+"&env="+env, function (error, response, body) {
             if (error) {
                 console.log(error)
             } else {
@@ -28,7 +42,7 @@ request(host + "repo/usrs/"+usr_id+"/suprlays", function (error, response, body)
                         return console.log(err);
                     }
                     //get platforms
-                    request(host + "repo/usrs/"+usr_id+"/platfrms", function (error, response, body) {
+                    request(host + "repo/usrs/"+usr_id+"/platfrms?axs_key="+token+"&env="+env, function (error, response, body) {
                         if (error) {
                             console.log(error)
                         } else {
@@ -39,7 +53,7 @@ request(host + "repo/usrs/"+usr_id+"/suprlays", function (error, response, body)
                                     return console.log(err);
                                 }
                                 //get componentes
-                                request(host + "repo/usrs/"+usr_id+"/comps", function (error, response, body) {
+                                request(host + "repo/usrs/"+usr_id+"/comps?axs_key="+token+"&env="+env, function (error, response, body) {
                                     if (error) {
                                         console.log(error)
                                     } else {
@@ -56,7 +70,7 @@ request(host + "repo/usrs/"+usr_id+"/suprlays", function (error, response, body)
                                                 return console.log(err);
                                             }
                                             //procs
-                                            request(host + "repo/usrs/"+usr_id+"/procs", function (error, response, body) {
+                                            request(host + "repo/usrs/"+usr_id+"/procs?axs_key="+token+"&env="+env, function (error, response, body) {
                                                 if (error) {
                                                     console.log(error)
                                                 } else {
