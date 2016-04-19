4e972f8badb093a5f13cc4ee289db0ef167f04f5
Miguel Celedon <miguelceledon@outlook.com>
2016-04-19 19:08:47 -0430

    Fixed header size

--- clair/scripts/generate_logos.bat
+++ clair/scripts/generate_logos.bat
@@ -11,6 +11,6 @@ FOR /f %%I IN ('dir /b') DO "C:\Program Files\Inkscape\inkscape.exe" --export-pn
 FOR /f %%I IN ('dir /b') DO "C:\Program Files\Inkscape\inkscape.exe" --export-png=..\mini\%%~nI.png --export-dpi=22 %%I
 
 CD "C:\fermat-org\clair\images\headers\svg"
-FOR /f %%I IN ('dir /b') DO "C:\Program Files\Inkscape\inkscape.exe" --export-png=..\high\%%~nI.png --export-dpi=720 %%I
+FOR /f %%I IN ('dir /b') DO "C:\Program Files\Inkscape\inkscape.exe" --export-png=..\high\%%~nI.png --export-dpi=360 %%I
 FOR /f %%I IN ('dir /b') DO "C:\Program Files\Inkscape\inkscape.exe" --export-png=..\medium\%%~nI.png --export-dpi=90 %%I
 FOR /f %%I IN ('dir /b') DO "C:\Program Files\Inkscape\inkscape.exe" --export-png=..\small\%%~nI.png --export-dpi=45 %%I
