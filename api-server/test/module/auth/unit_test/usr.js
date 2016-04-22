278a9ac427444aa77fef2538558e65254432635b
Luis Campo <campusprize@gmail.com>
2016-04-22 10:49:02 -0430

    fix #515 Test insert user with permission

--- api-server/test/module/auth/unit_test/usr.js
+++ api-server/test/module/auth/unit_test/usr.js
@@ -3,21 +3,22 @@ var mongoose = require('mongoose');
 var assert = require('assert');
 var usrNm = null;
 var email = null;
+var permUsr = 77000;
 describe('User', function() {
 	before(function() {
 		console.log('info', 'Starting database connexion');
 		var db = require('../../../../db');
 	});
 
-	describe('#delAllUsrs()', function() {
+	/*describe('#delAllUsrs()', function() {
 		it('should remove all users', function(done) {
 			usrMod.delAllUsrs(done);
 		});
-	});
+	});*/
 
 	describe('#insOrUpdUsr()', function() {
 		it('should save a user without error', function() {
-			usrMod.insOrUpdUsr('campol', 'campusprize@gmail.com', 'Luis', new Date(), 'maracaibo', 'avatar', new mongoose.Types.ObjectId(), 'url', 'bio', function(err, resp) {
+			usrMod.insOrUpdUsr('campol', 'campusprize@gmail.com', 'Luis', new Date(), 'maracaibo', 'avatar', new mongoose.Types.ObjectId(), 'url', 'bio', permUsr,function(err, resp) {
 				usrNm = resp.usrnm;
 				email = resp.email;
 				console.log("Usr", resp);
@@ -25,16 +26,16 @@ describe('User', function() {
 		});
 	});
 
-	describe('#getUsrs()', function() {
+	/*describe('#getUsrs()', function() {
 		it('should get a list of users', function() {
 			usrMod.getUsrs(function(err, resp) {
 				assert(resp.length > 0);
 				console.log("resp.length", resp.length);
 			});
 		});
-	});
+	});*/
 
-	describe('#getUsrsByEmail()', function() {
+	/*describe('#getUsrsByEmail()', function() {
 		it('should obtain a given user email', function() {
 			usrMod.getUsrsByEmail(email, function(err, resp) {
 				console.log("User by email "+email, resp);
@@ -48,5 +49,5 @@ describe('User', function() {
 				console.log("User by user name "+usrNm, resp);
 			});
 		});
-	});
+	});*/
 });
