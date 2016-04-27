edf765ab99b56cddc32c6a2a82ba34dbfb8f33e7
Miguel Celedon <miguelceledon@outlook.com>
2016-04-26 20:28:36 -0430

    Created support for clients

--- treant/treant.js
+++ treant/treant.js
@@ -3,6 +3,7 @@
 var map,
     markClusterer,
     nodes = [],
+    clients = [],
     infoWindow = null;
 
 $(document).ready(main);
@@ -18,13 +19,45 @@ function main() {
 function drawMap() {
     
     window.map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 45.64875223755158, lng: 5.864989237500002},
+        center: {lat: 14.695393959866866, lng: 9.029051737500042},
         zoom: 2
     });
     
     window.markClusterer = new MarkerClusterer(window.map);
+    window.map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(createControlPanel());
     
     getNodes();
+}
+
+function loadTestData() {
+    var list = [],
+        MAX_LAT = 50,
+        MAX_LNG = 150,
+        getLatLng = function() {
+            return { latitude: Math.random() * MAX_LAT * 2.5 - MAX_LAT, longitude : Math.random() * MAX_LNG * 2 - MAX_LNG};
+        },
+        getIP = function() {
+            return Math.floor(Math.random() * 255) +"."+ Math.floor(Math.random() * 255) +"."+ Math.floor(Math.random() * 255) +"."+ Math.floor(Math.random() * 255);
+        },
+        getNumber = function(){
+            return Math.floor(Math.random() * 30);
+        };
+    
+    for(var i = 0; i < 500; i++) {
+        var location = getLatLng();
+        location.ip = getIP();
+        
+        list.push({
+            extra : {
+                location : location,
+                current : {
+                    registeredClientConnection : getNumber()
+                }
+            }
+        });
+    }
+    
+    return list;
 }
 
 /**
@@ -37,12 +70,38 @@ function getNodes() {
         url : window.helper.getAPIUrl("servers"),
         method: "GET",
         crossDomain: true,
-        success : drawNodes,
+        success : function(list) {
+            createNodes(list);
+            //createClients(list);
+        },
         error : function(request, error) {
             window.alert("Could not retrieve the data, see console for details.");
             window.console.dir(error);
         }
     });
+    
+//    createNodes(loadTestData());
+//    createClients(loadTestData());
+}
+
+function createClients(list) {
+    var setListener = function(node) {
+        node.marker.addListener('click', function() {drawDetails(node);});
+    };
+    
+    for(var i = 0; i < list.length; i++) {
+        var node = list[i];
+        
+        var marker = new google.maps.Marker({
+            label : "Client",
+            position : {lat : node.extra.location.latitude, lng : node.extra.location.longitude}
+        });
+        
+        node.marker = marker;
+        clients.push(node);
+        
+        setListener(node);
+    }
 }
 
 /**
@@ -50,7 +109,7 @@ function getNodes() {
  * @author Miguelcldn
  * @param {Array} list Response from the server
  */
-function drawNodes(list) {
+function createNodes(list) {
     
     var setListener = function(node) {
         node.marker.addListener('click', function() {drawDetails(node);});
@@ -81,11 +140,22 @@ function drawDetails(node) {
     
     if(infoWindow !== null) infoWindow.close();
     
-    infoWindow = new google.maps.InfoWindow({
-        content : "<div>" +
+    var content = "";
+    
+    if(node.marker.label === "Node") {
+        content = "<div>" +
         "<strong>IP:</strong> " + node.extra.location.ip + "<br/>" +
         "<strong>Registered client connections:</strong> " + node.extra.current.registeredClientConnection + "<br/>" +
-        "</div>"
+        "</div>";
+    }
+    else {
+        content = "<div>" +
+        "<strong>IP:</strong> " + node.extra.location.ip + "<br/>" +
+        "</div>";
+    }
+    
+    infoWindow = new google.maps.InfoWindow({
+        content : content
     });
     
     infoWindow.open(map, node.marker);
