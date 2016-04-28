cdddbca8b0516f8ed81178bc1e32f0b8d4b0373e
Luis Campo <campusprize@gmail.com>
2016-04-28 10:58:24 -0430

    Insert users for test  update of user permissions

--- api-server/test/module/auth/unit_test/usr.js
+++ api-server/test/module/auth/unit_test/usr.js
@@ -4,6 +4,8 @@ var assert = require('assert');
 var usrNm = null;
 var email = null;
 var permUsr = 77000;
+var _INTERVAL = 2000;
+var loop = 0;
 describe('User', function() {
 	before(function() {
 		console.log('info', 'Starting database connexion');
@@ -15,14 +17,30 @@ describe('User', function() {
 			usrMod.delAllUsrs(done);
 		});
 	});*/
-
+	var usrs = [
+		'darkespriest',
+		'Rart3001',
+		'lnacosta',
+		'furszy',
+		'acostarodrigo',
+		'Luis-Fernando-Molina',
+		'Miguelcldn',
+		'emmanuelcolina',
+		'IsaTab',
+		'ricardo460',
+		'simonorono',
+		'kyxer',
+		'fuelusumar',
+		'nelsonalfo'
+	];
 	describe('#insOrUpdUsr()', function() {
 		it('should save a user without error', function() {
-			usrMod.insOrUpdUsr('campol', 'campusprize@gmail.com', 'Luis', new Date(), 'maracaibo', 'avatar', new mongoose.Types.ObjectId(), 'url', 'bio', permUsr,function(err, resp) {
-				usrNm = resp.usrnm;
-				email = resp.email;
-				console.log("Usr", resp);
-			});
+				console.log("Usr", loop);
+					usrMod.insOrUpdUsr('carlucho', 'carlucho' + '@gmail.com','carlucho', new Date(), 'maracaibo', 'avatar', new mongoose.Types.ObjectId(), 'url', 'bio', permUsr, function(err, resp) {
+						usrNm = resp.usrnm;
+						email = resp.email;
+						console.log("Response ", resp);
+					});
 		});
 	});
 
