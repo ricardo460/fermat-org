877793bdb4e3b51894e025d9cbece457926fab51
Luis Campo <campusprize@gmail.com>
2016-06-01 17:59:04 -0400

    #566 Return response when converter svg

--- api-server/modules/repository/lib/svg2png.js
+++ api-server/modules/repository/lib/svg2png.js
@@ -13,6 +13,7 @@ exports.svgToPng = function(path, filename, callback) {
 			.then(svg2png)
 			.then(buffer => fs.writeFile("./uploads/"+filename+".png", buffer))
 			.catch(e => console.error(e));
+			return callback(null, "Conversion completed successfully");
 	} catch (err) {
 		console.log("Error: " + err);
 		return callback(err, null);
