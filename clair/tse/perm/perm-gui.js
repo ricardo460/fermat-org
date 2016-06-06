fe4ed6cbda193580124d91f1a168c41d0aa133f7
IsaTab <isaiasve30@hotmail.com>
2016-06-06 16:52:47 -0400

    TSE gui update

--- clair/tse/perm/perm-gui.js
+++ clair/tse/perm/perm-gui.js
@@ -2,6 +2,8 @@ var user_data = getUserID(),
     axs_key = '',
     environment = '',
     perm = 00000,
+    usertype = 'developer',
+    reference,
     userList;
 //global constants
 var SERVER = 'http://api.fermat.org';
@@ -33,17 +35,31 @@ function init() {
         environment = 'development';
         axs_key = user_data.axs_key;
 
-        getUsers();
+        updateUserList(true);
 
-        setTimeout(function (){
-                document.getElementById('spinner').style.display = 'none';
-                $('#users').removeClass('hidden');
-                tagPermissions("platform");
-                tagPermissions("superlayer");
-                tagPermissions("layer");
-                $('#perm').removeClass('hidden');
-        }, 7000);
+        $('#changePermissions').click(function() {
+                verify();
+        });
     //}
+}
+
+function updateUserList(reload){
+    var table = document.getElementById("users");
+    while(table.rows.length > 1) {
+        table.deleteRow(1);
+    }
+    getUsers();
+
+    if(reload !== true){
+        setTimeout(function (){
+            document.getElementById('spinner').style.display = 'none';
+            $('#users').removeClass('hidden');
+            tagPermissions("platform");
+            tagPermissions("superlayer");
+            tagPermissions("layer");
+            $('#perm').removeClass('hidden');
+        }, 7000);
+    }
 }
 
 function getUsers(){
@@ -105,9 +121,168 @@ function comparePermissions(code){
 
 function fillTable(data){
     if(data.name !== null)
-        $('#users').append("<tr><td>" + data.usrnm + "</td><td>" + data.name + "</td><td>" + "<button id='" + data._id + "' name='perm: " + data.usrnm + "' onclick=''>Modify</button>" + "</td></tr>");
+        $('#users').append("<tr><td>" + data.usrnm + "</td><td>" + data.name + "</td><td>" + "<button id='" + data.usrnm + "' name=" + data.usrnm + " onclick='modifyPermissions(this)'>Modify</button>" + "</td></tr>");
     else
-        $('#users').append("<tr><td>" + data.usrnm + "</td><td>" + "</td><td>" + "<button id='" + data._id + "' name='perm: " + data.usrnm + "' onclick=''>Modify</button>" + "</td></tr>");
+        $('#users').append("<tr><td>" + data.usrnm + "</td><td>" + "</td><td>" + "<button id='" + data.usrnm + "' name=" + data.usrnm + " onclick='modifyPermissions(this)'>Modify</button>" + "</td></tr>");
+}
+
+function listVisibility(action){
+    if(action === 'hide'){
+        $('#users').addClass('hidden');
+        $('#perm-form').removeClass('hidden');
+    }
+    else{
+        $('#users').removeClass('hidden');
+        $('#perm-form').addClass('hidden');
+    }
+}
+
+function cancel(){
+    listVisibility('show');
+    document.getElementById("platform-add").disabled = false;
+    document.getElementById("platform-mod").disabled = false;
+    document.getElementById("platform-del").disabled = false;
+    document.getElementById("sl-add").disabled = false;
+    document.getElementById("sl-mod").disabled = false;
+    document.getElementById("sl-del").disabled = false;
+    document.getElementById("layer-add").disabled = false;
+    document.getElementById("layer-mod").disabled = false;
+    document.getElementById("layer-del").disabled = false;
+    document.getElementById("platform-add").checked = false;
+    document.getElementById("platform-mod").checked = false;
+    document.getElementById("platform-del").checked = false;
+    document.getElementById("sl-add").checked = false;
+    document.getElementById("sl-mod").checked = false;
+    document.getElementById("sl-del").checked = false;
+    document.getElementById("layer-add").checked = false;
+    document.getElementById("layer-mod").checked = false;
+    document.getElementById("layer-del").checked = false;
+}
+
+function modifyPermissions(element){
+    listVisibility('hide');
+    reference = element.id;
+    var url = getRoute('perm', element.id);
+
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).success (
+        function (res) {
+            var digit,
+                codeDigit,
+                temp = parseInt(res.perm);
+
+            digit = Math.floor((temp % 11000) / 100);
+            codeDigit = Math.floor((perm % 11000) / 100);
+            enable(digit, 'platform', codeDigit);
+
+            digit = Math.floor((temp % 11100) / 10);
+            codeDigit = Math.floor((perm % 11100) / 10);
+            enable(digit, 'sl', codeDigit);
+                
+            digit = Math.floor((Math.floor(temp % 1000)) % 10);
+            codeDigit = Math.floor((Math.floor(perm % 1000)) % 10);
+            enable(digit, 'layer', codeDigit);
+        }
+    );
+}
+
+function enable(digit, structure, compareDigit) {
+    if(digit % 2 === 1){
+        document.getElementById(structure+"-del").checked = true;
+        document.getElementById(structure+"-del").disabled = true;
+    }
+    else{
+        if(compareDigit % 2 === 0)
+            document.getElementById(structure+"-del").disabled = true;   
+    }
+
+    digit = Math.floor(digit / 2);
+
+    if(digit % 2 === 1){
+        document.getElementById(structure+"-mod").checked = true;
+        document.getElementById(structure+"-mod").disabled = true;   
+    }
+    else{
+        compareDigit = Math.floor(compareDigit / 2);
+        if(compareDigit % 2 === 0)
+            document.getElementById(structure+"-mod").disabled = true;
+    }
+
+    if(Math.floor(digit / 2) === 1){
+        document.getElementById(structure+"-add").checked = true;
+        document.getElementById(structure+"-add").disabled = true;
+    }
+    else{
+        if(Math.floor(compareDigit / 2) === 0)
+            document.getElementById(structure+"-add").disabled = true;
+    }
+}
+
+function binaryToOctal(structure){
+    var digit,
+        digitAdd,
+        digitMod,
+        digitDel;
+
+    if(document.getElementById(structure+"-add").checked)
+        digitAdd = 1;
+    else
+        digitAdd = 0;
+    
+    if(document.getElementById(structure+"-mod").checked)
+        digitMod = 1;
+    else
+        digitMod = 0;
+
+    if(document.getElementById(structure+"-del").checked)
+        digitDel = 1;
+    else
+        digitDel = 0;
+
+    digit = (digitAdd * 4) + (digitMod * 2) + (digitDel * 1);
+    return digit;
+}
+
+function verify(){
+    var digitPlatform,
+        digitSuperlayer,
+        digitLayer,
+        url;
+
+    digitPlatform = binaryToOctal('platform');
+    digitSuperlayer = binaryToOctal('sl');
+    digitLayer = binaryToOctal('layer');
+
+    url = getRoute('change', user_data._id);
+
+    digitPlatform = (77000 + (digitPlatform * 100) + (digitSuperlayer * 10) + digitLayer);
+
+    var data = {
+        usrnm : reference,
+        perm: digitPlatform.toString(),
+        usr_id: user_data._id
+    };
+
+    console.log(data);
+    console.log(url);
+
+    $.ajax({
+            url: url,
+            method: "POST",
+            data: data
+    }).success (
+        function (res) {
+            window.alert("Permissions modified successfully");
+            updateUserList();
+            cancel();
+        }
+    ).error (
+        function (xhr, status, error) {
+            window.alert(xhr.responseText);
+        }
+    );
 }
 
 function getRoute(route, user){
@@ -118,9 +293,11 @@ function getRoute(route, user){
 
     if(route === 'users')
         tail = "/v1/repo/devs";
-    else
+    if(route === 'perm')
         tail = "/v1/user/" + user;
-    
+    if(route === 'change')
+        tail = "/v1/user/" + user + "/changePerms";
+
     param = {
         env : environment,
         axs_key : axs_key
--- clair/tse/tse.js
+++ clair/tse/tse.js
@@ -2,12 +2,13 @@
 var user_data = getUserID(),
     axs_key = '',
     environment = '',
-    current = 'layer',
+    current = 'platform',
     request = 'add',
     referenceName = '',
     referenceId = '',
     referenceCode = '',
     referenceOrder = '',
+    usertype = 'developer',
     perm = 77000;
 //global constants
 var SERVER = 'http://api.fermat.org';
@@ -40,6 +41,8 @@ function init() {
         updateList('layer',true);
         updateList('platform',true);
         updateList('superlayer',true);
+
+        sel();
 
         $(document).ready(function() {
 
@@ -117,14 +120,14 @@ function deleteStructure(element, type){
             updateList(type, true);
         }, 3000);
 
-        clearGroupForm();
+        clearGroupForm(usertype);
         clearLayerForm();
     }
 }
 
 function modifyStructure(element, type){
 
-    add();
+    add('modify');
     var url;
 
     request = 'modify';
@@ -146,15 +149,17 @@ function modifyStructure(element, type){
         method: "GET"
     }).success (
         function (res) {
+
+            referenceName = res.name;
+            referenceOrder = res.order;
+            referenceId = element.id;
+            
             if(type === 'layer'){
 
                 document.getElementById('nextName').style.display = 'block';
                 document.getElementById("layerName").value = res.name.capitalize();
                 document.getElementById("layerLang").value = res.lang;
 
-                referenceName = res.name;
-                referenceOrder = res.order;
-                referenceId = element.id;
                 $("#layerOrder").empty();
 
                 if(res.suprlay === false){
@@ -169,25 +174,28 @@ function modifyStructure(element, type){
                 findPosition(type, res.order);
             }
             else{
+                if(usertype === 'developer'){
+                    document.getElementById("groupCode").value = res.code;
+                    document.getElementById("groupName").value = res.name.capitalize();
 
-                document.getElementById("groupCode").value = res.code;
-                document.getElementById("groupName").value = res.name.capitalize();
+                    var list = document.getElementById("groupDeps"),
+                        l = list.options.length;
 
-                var list = document.getElementById("groupDeps"),
-                    l = list.options.length;
-
-                for(var e in res.deps){
-                    for(var i = 0; i < l; i++){
-                        if(res.deps[e] === list.options[i].value)
-                            list.options[i].selected = 'true';
+                    for(var e in res.deps){
+                        for(var i = 0; i < l; i++){
+                            if(res.deps[e] === list.options[i].value)
+                                list.options[i].selected = 'true';
+                        }
                     }
-                }
-                referenceName = res.name;
-                referenceId = element.id;
-                referenceCode = res.code;
-                referenceOrder = res.order;
+                    retrieveData(current, current, null);
+                    referenceCode = res.code;
 
-                findPosition(type, res.order);
+                    findPosition(type, res.order);
+                }
+                else{
+                    document.getElementById("desCode").value = res.code;
+                    document.getElementById("desName").value = res.name.capitalize();
+                }
             }
         }
     );
@@ -218,7 +226,7 @@ function findPosition(type, order){
              var l = res.length;
 
              for(var i = 0; i < l; i++){
-                if(res[i].order === order && order === 0){
+                if(res[i].order === order && order === 1){
                     if(type === "layer"){
                             if(res[1].suprlay !== false)
                                 document.getElementById("layerNext").innerHTML = "Currently: Above - " + res[1].name.capitalize() + " (In Superlayer: " + res[1].suprlay + ")";
@@ -273,7 +281,6 @@ function updateList(list, refresh){
         $("#groupOrder").empty();
         $("#groupDeps").empty();
         retrieveData("platform", null);
-        retrieveData("platform", "platform", null);
         retrieveData("superlayer", "platform", null);
     }
     else{
@@ -285,7 +292,6 @@ function updateList(list, refresh){
         $("#groupOrder").empty();
         $("#groupDeps").empty();
         retrieveData("superlayer", null);
-        retrieveData("superlayer", "superlayer", null);
         retrieveData("platform", "superlayer", null);
         retrieveData("superlayer", "layers", null);
     }
@@ -304,7 +310,23 @@ function updateList(list, refresh){
     }
 }
 
-function add(){
+function clearReference(){
+    referenceName = '';
+    referenceId = '';
+    referenceCode = '';
+    referenceOrder = '';
+}
+
+function add(option){
+    
+    if(option !== 'modify'){
+        clearReference();
+        if(current === 'layer')
+            retrieveData(current, 'layers', false);
+        else
+            retrieveData(current, current, null);
+    }
+
     hideLists();
     $('#type').prop('disabled', true);
     $('#add').prop('disabled', true);
@@ -313,18 +335,19 @@ function add(){
         showForm('#layerForm');
         clearLayerForm();
     } else {
-        if(current === 'platform')
-            retrieveData("platforms", "group", null);
-        else
-            retrieveData("superlayers", "group", null);
-        showForm('#groupForm');
-        clearGroupForm();
+        if(usertype === 'developer'){
+            clearGroupForm(usertype);
+            showForm('#groupForm');
+        }
+        else{
+            showForm('#desForm');
+        }
     }
     request = 'add';
 }
 
 function cancel() {
-    clearGroupForm();
+    clearGroupForm(usertype);
     clearLayerForm();
     hideForm(getActiveForm());
     $('#type').prop('disabled', false);
@@ -336,7 +359,10 @@ function getActiveForm() {
     if(current === 'layer') {
         return $('#layerForm');
     } else {
-        return $('#groupForm');
+        if(usertype === 'developer')
+            return $('#groupForm');
+        else
+            return $('#desForm');
     }
 }
 
@@ -345,12 +371,10 @@ function sel() {
         $('#layerList').addClass('hidden');
         $('#superlayerList').removeClass('hidden');
         $('#platformList').addClass('hidden');
-        updateList(current);
     } else if (current === 'platform') {
         $('#layerList').addClass('hidden');
         $('#superlayerList').addClass('hidden');
         $('#platformList').removeClass('hidden');
-        updateList(current);
     } else {
         $('#layerList').removeClass('hidden');
         $('#superlayerList').addClass('hidden');
@@ -358,15 +382,26 @@ function sel() {
     }
 }
 
-function clearGroupForm() {
-    document.getElementById('groupCode').value = '';
-    document.getElementById('groupName').value = '';
+function clearGroupForm(type) {
+    if(type === 'developer'){
+        document.getElementById('groupCode').value = '';
+        document.getElementById('groupName').value = '';
+        document.getElementById('groupDeps').value = '';
+        $("#groupOrder").empty();
+    }
+    else{
+        document.getElementById('desCode').value = '';
+        document.getElementById('desName').value = '';
+        document.getElementById('desHeader').value = '';
+        document.getElementById('desIcon').value = '';
+    }
 }
 
 function clearLayerForm() {
     document.getElementById('layerName').value = '';
     document.getElementById('layerNext').innerHTML = '';
     document.getElementById('nextName').style.display = 'none';
+    $("#layerOrder").empty();
 }
 
 function hideLists() {
@@ -553,7 +588,8 @@ function verify(form, request){
                 url = getRoute(repo, "insert");
                 sendRequest(url, 'POST', data, form);
                 updateList(form, false);
-                clearGroupForm();
+                clearGroupForm(usertype);
+                retrieveData(current, current, null);
             }
         }
     }
@@ -638,7 +674,8 @@ function verify(form, request){
                     updateList(form, true);
                 }, 3000);
 
-                clearGroupForm();
+                clearGroupForm(usertype);
+                retrieveData(current, current, null);
                 clearLayerForm();
             }
         }
