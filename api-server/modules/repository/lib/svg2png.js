9acd96191fb52984fc8eb91a9f13304d2ef46000
Luis Campo <campusprize@gmail.com>
2016-06-08 05:52:26 -0400

    fix #559 svg upload to the server and convert to png with different dpi

--- api-server/modules/repository/lib/svg2png.js
+++ api-server/modules/repository/lib/svg2png.js
@@ -6,37 +6,152 @@
 /* global which */
 require('shelljs/global');
 var path = require('path');
+var JSFtp = require("jsftp");
+var fs = require("fs");
+var filesToSend = [];
+var cwd = process.cwd();
+var dir = path.join(cwd, 'uploads');
+var ftpBitdubai = new JSFtp({
+	host: "bitdubai.com",
+	port: 21, // defaults to 21 
+	user: "navi@bitdubai.com", // defaults to "anonymous" 
+	pass: "Dfg15bLP%4L4" // defaults to "@anonymous" 
+});
+var ftpFermat = new JSFtp({
+	host: "fermat.org",
+	port: 21, // defaults to 21 
+	user: "navi@fermat.org", // defaults to "anonymous" 
+	pass: "Dfg15bLP%4L4" // defaults to "@anonymous" 
+});
+/**
+ * [convert description]
+ * @param  {[type]} filename [description]
+ * @param  {[type]} dpi      [description]
+ * @param  {[type]} dir      [description]
+ * @return {[type]}          [description]
+ */
+var convert = function(path_png, name_svg, dpi, dir) {
+	console.log('test', test('-d', dir));
+	if (test('-d', dir)) {
+		cd(dir);
+		console.log('path_png', path_png);
+		console.log('name_svg', name_svg);
+		if (exec('inkscape -e ' + path_png + ' -d ' + dpi + ' ' + name_svg + '.svg').code !== 0) {
+			echo('Error: You can not perform the conversion');
+			throw new Error('You can not perform the conversion');
+			//res.status(402).send("You can not perform the conversion");
+		} else return "Conversion completed successfully";
+		//else res.status(200).send("ok");
+	}
+};
+/**
+ * [createDir description]
+ * @param  {[type]} path [description]
+ * @return {[type]}      [description]
+ */
+var createDir = function(path_, qt_img) {
+	console.log('path ', path_);
+	try {
+		fs.accessSync(path_, fs.F_OK);
+		console.log('return path ', '/' + qt_img);
+		return '/' + qt_img;
+	} catch (e) {
+		try {
+			console.log('creating dir ');
+			fs.mkdirSync(path_);
+			console.log('dir created return path ', '/' + qt_img);
+			return '/' + qt_img;
+		} catch (e) {
+			console.error('Error', e);
+			if (e.code !== 'EEXIST') throw e;
+		}
+	}
+};
 /**
  * [svgToPng description]
  * @param  {[type]}   url_svg  [description]
  * @param  {Function} callback [description]
  * @return {[type]}            [description]
  */
-var svgToPng = function(type, filename, dpi, callback) {
-	var cwd = process.cwd();
-	var dir = path.join(cwd, 'uploads');
+var svgToPng = function(type, filename, callback) {
+	var qtGroupAndType = [{
+		quality: 'high',
+		dpi: 720
+	}, {
+		quality: 'medium',
+		dpi: 90
+	}, {
+		quality: 'small',
+		dpi: 45
+	}, {
+		quality: 'mini',
+		dpi: 22
+	}];
+	var qtHeader = [{
+		quality: 'high',
+		dpi: 360
+	}, {
+		quality: 'medium',
+		dpi: 90
+	}, {
+		quality: 'small',
+		dpi: 45
+	}];
+	var filePng = "",
+		pathF = "",
+		pathV = "";
+	var i = 0;
 	try {
-		//console.log('dir', dir);
-		//console.log('fileName', filename.split('.'));
+		console.log('type', type);
+		filesToSend.push(dir + '/' + filename);
 		filename = filename.split('.')[0];
-		if (test('-d', dir)) {
-			cd(dir);
-			if (exec('inkscape -e ' + filename + '.png -d ' + dpi + ' ' + filename + '.svg').code !== 0) {
-				echo('Error: You can not perform the conversion');
-				//res.status(402).send("You can not perform the conversion");
-			} else return callback(null, "Conversion completed successfully");
-			//else res.status(200).send("ok");
+		if (type === 'group' || type === 'type') {
+			for (i = 0; i < qtGroupAndType.length; i++) {
+				filePng = filename + ".png";
+				console.log('dirF', dir);
+				pathF = path.join(dir, qtGroupAndType[i].quality);
+				console.log('pathF', pathF);
+				pathV = createDir(pathF, qtGroupAndType[i].quality);
+				filesToSend.push(pathF + '/' + filePng);
+				convert('.' + pathV + '/' + filePng, filename, qtGroupAndType[i].dpi, dir);
+			}
+		} else {
+			for (i = 0; i < qtHeader.length; i++) {
+				filePng = filename + ".png";
+				pathF = path.join(dir, qtHeader[i].quality);
+				pathV = createDir(pathF, qtHeader[i].quality);
+				filesToSend.push(pathF + '/' + filePng);
+				convert('.' + pathV + '/' + filePng, filename, qtHeader[i].dpi, dir);
+			}
 		}
+		return callback(null, 'Conversion completed successfully');
 	} catch (err) {
 		console.log("Error: " + err);
 		return callback(err, null);
 	}
 };
 
-exports.pushFtp = function(type, filename, dpi, callback) {
-	var dpiGroup = [720, 90, 45, 22];
+exports.pushFtp = function(type, filename, callback) {
 	try {
+		svgToPng(type, filename, function(err, res) {
+			if (err) return callback(err, null);
+			if (res) {
+				console.log('filesToSend', filesToSend);
+				for (var i = 0; i < filesToSend.length; i++) {
+					ftpBitdubai.put(filesToSend[i], 'path/to/remote/file.txt',
+						function(hadError) {
+							if (!hadError)
+								console.log("File transferred successfully!");
+							else {
+								console.log("Error transferring file");
+								return callback(hadError, null);
+							}
+						});
+				}
+				return callback(null, 'Files transferred successfully!');
+			}
 
+		});
 	} catch (err) {
 		console.log("Error: " + err);
 		return callback(err, null);
--- api-server/routes/v1/svg/index.js
+++ api-server/routes/v1/svg/index.js
@@ -3,17 +3,24 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var security = require('../../../lib/utils/security');
+var cwd = process.cwd();
+var path = require('path');
+var dir = path.join(cwd, 'uploads');
 var storage = multer.diskStorage({
 	destination: function(req, file, callback) {
-		callback(null, './uploads');
+		//callback(null, './uploads');
+		console.log('dir', dir);
+		callback(null, dir);
 	},
 	filename: function(req, file, callback) {
-		callback(null, file.fieldname + '-' + Date.now() + '.svg');
+		//callback(null, file.fieldname + '-' + Date.now() + '.svg');
+		callback(null, file.originalname);
 	}
 });
 var upload = multer({
 	storage: storage
 }).single('svg');
+
 /**
  * @api {post} /v1/svg/upload/:type upload svg file
  * @apiName Upload
@@ -24,7 +31,7 @@ var upload = multer({
  */
 router.post('/upload/:type', function(req, res, next) {
 	try {
-		//console.log(req);
+		console.log(req.body);
 		if (!security.isValidData(req.params.type)) {
 			res.status(412).send({
 				"message": "missing or invalid data"
@@ -33,19 +40,19 @@ router.post('/upload/:type', function(req, res, next) {
 			console.log("execute route /upload");
 			upload(req, res, function(err) {
 				if (err) {
+					console.log(err + ": Error uploading file.");
 					return res.end(err + ": Error uploading file.");
 				}
 				var fileName = req.file.filename;
-				console.log("fileName", fileName);
-				svg2png.svgToPng(req.body.type, fileName, function(err, resp) {
-					if (err) res.status(402).send("You can not perform the conversion");
+				svg2png.pushFtp(req.params.type, fileName, function(err, resp) {
+					if (err) res.status(402).send(err +": You can not perform the conversion");
 					if (resp) res.status(200).send(resp);
 				});
 			});
 		}
 	} catch (err) {
 		console.error("Error", err);
-		res.status(402).send("You can not perform the conversion");
+		res.status(402).send(err+": You can not perform the conversion");
 	}
 });
 // router export
