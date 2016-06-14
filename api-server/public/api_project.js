a22f20e6009d62c25b93d7f988b8c632cad14b6f
Miguel Celedon <miguelceledon@outlook.com>
2016-06-14 17:52:07 -0400

    build 1465940933031

--- api-server/public/api_project.js
+++ api-server/public/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-06-14T20:31:09.481Z",
+    "time": "2016-06-14T21:51:03.737Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
