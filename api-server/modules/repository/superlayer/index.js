b93f1feef15710bdd85907f8d9a1b14ba6a0850e
Luis Campo <campusprize@gmail.com>
2016-06-22 14:28:47 -0400

    #609 get dependencies platform with codes and validate dependencies platform id exist in database

--- api-server/modules/repository/superlayer/index.js
+++ api-server/modules/repository/superlayer/index.js
@@ -2,6 +2,8 @@ var suprlaySrv = require('./services/suprlay');
 var SuprlayMdl = require('./models/suprlay');
 var compMod = require('../component');
 var orderLib = require('../../../lib/utils/order');
+var MapsCodesPlatfSuprl = require("../lib/codes_platf_suprlay");
+var mapsCodes = new MapsCodesPlatfSuprl();
 /**
  * [sort description]
  *
@@ -48,6 +50,22 @@ var getOrdrLstSuprlays = function(callback) {
 		callback(err, null);
 	}
 };
+var existDeps = function(deps) {
+	var platfrmCode = null;
+	var suprlayCode = null;
+	var notExis = {};
+	for (var i = 0; i < deps.length; i++) {
+		platfrmCode = mapPlatfrms[deps[i]];
+		suprlayCode = mapSuprlays[deps[i]];
+		if ((platfrmCode === undefined || platfrmCode === null) && (suprlayCode === undefined || suprlayCode === null)) {
+			notExis._id = deps[j];
+			notExis.valid = true;
+			return notExis;
+		}
+	}
+	notExis.valid = false;
+	return notExis;
+};
 /**
  * [insOrUpdSuprlay description]
  *
@@ -65,6 +83,7 @@ var getOrdrLstSuprlays = function(callback) {
  */
 exports.insOrUpdSuprlay = function(code, name, logo, deps, order, callback) {
 	'use strict';
+	var existDeps = null;
 	try {
 		//order = code ? getOrder(code) : null;
 		suprlaySrv.findSuprlayByCode(code, function(err_supr, res_supr) {
@@ -86,6 +105,8 @@ exports.insOrUpdSuprlay = function(code, name, logo, deps, order, callback) {
 						deps = deps.replace(" ", "").split(',');
 					else
 						deps = [];
+					existDeps = existDeps(deps);
+					if (existDeps.valid) return callback('Dependencies id ' + existDeps._id + ' not found');
 					set_obj.deps = deps;
 					res_supr.deps = deps;
 				}
@@ -123,6 +144,8 @@ exports.insOrUpdSuprlay = function(code, name, logo, deps, order, callback) {
 					deps = [];
 				else
 					deps = deps.split(',');
+				existDeps = existDeps(deps);
+				if (existDeps.valid) return callback('Dependencies id ' + existDeps._id + ' not found');
 				if (order === undefined || order === null) getOrdrLstSuprlays(function(err, nu_order) {
 					if (err) return callback(err, null);
 					if (nu_order) {
@@ -182,6 +205,18 @@ exports.getSuprlays = function(callback) {
 			if (err) {
 				callback(err, null);
 			} else {
+				var mapPlatfrms = mapsCodes.mapsCodePlatfrm();
+				var mapSuprlays = mapsCodes.mapsCodeSuprlay();
+				for (var i = 0; i < suprlays.length; i++) {
+					for (var j = 0; j < suprlays[i].deps.length; j++) {
+						var platfrmCode = mapPlatfrms[suprlays[i].deps[j]];
+						var suprlayCode = mapSuprlays[suprlays[i].deps[j]];
+						if (platfrmCode)
+							suprlays[i].deps[j] = platfrmCode;
+						else if (suprlayCode)
+							suprlays[i].deps[j] = suprlayCode;
+					}
+				}
 				callback(null, suprlays);
 			}
 		});
@@ -226,6 +261,16 @@ exports.findSuprlayById = function(_id, callback) {
 		if (err_suprlay) {
 			return callback(err_suprlay, null);
 		} else if (res_suprlay) {
+			var mapPlatfrms = mapsCodes.mapsCodePlatfrm();
+			var mapSuprlays = mapsCodes.mapsCodeSuprlay();
+			for (var j = 0; j < res_suprlay.deps.length; j++) {
+				var platfrmCode = mapPlatfrms[res_suprlay.deps[j]];
+				var suprlayCode = mapSuprlays[res_suprlay.deps[j]];
+				if (platfrmCode)
+					res_suprlay.deps[j] = platfrmCode;
+				else if (suprlayCode)
+					res_suprlay.deps[j] = suprlayCode;
+			}
 			return callback(null, res_suprlay);
 		} else {
 			return callback(null, null);
@@ -252,6 +297,7 @@ exports.updateSuprlayById = function(_sprly_id, code, name, logo, deps, order, c
 	'use strict';
 	try {
 		var set_obj = {};
+		var existDeps = null;
 		if (code) {
 			set_obj.code = code;
 		}
@@ -263,6 +309,8 @@ exports.updateSuprlayById = function(_sprly_id, code, name, logo, deps, order, c
 		}
 		if (deps) {
 			deps = deps.split(',');
+			existDeps = existDeps(deps);
+			if (existDeps.valid) return callback('Dependencies id ' + existDeps._id + ' not found');
 			set_obj.deps = deps;
 		}
 		if (typeof order != "undefined") {
