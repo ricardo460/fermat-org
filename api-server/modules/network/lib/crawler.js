f84a4924c9db5d95a341a896162b8ad7d86534b4
Miguel Celedon <miguelceledon@outlook.com>
2016-06-28 16:50:23 -0400

    Updated crawling

--- api-server/modules/network/lib/crawler.js
+++ api-server/modules/network/lib/crawler.js
@@ -7,155 +7,213 @@ var SHA256 = require("crypto-js/sha256");
 var config = require('../../../config');
 var servMod = require('../server');
 var clintMod = require('../client');
-/**
- * [doLogin description]
- *
- * @method doLogin
- *
- * @param  {Function} callback [description]
- *
- * @return {[type]}   [description]
- */
-var doLogin = function(callback) {
-    var credentials = {
-        user: config.network.username,
-        password: SHA256(config.network.password) + ''
-    };
-    var options = {
-        url: 'http://' + config.network.ip + ':9090/fermat/api/user/login',
-        method: 'POST',
-        body: credentials,
-        json: true
-    };
-    request(options, function(error, response, body) {
-        if (error) return callback(error, null);
-        if (body && body.success && body.authToken) {
-            var authRequest = request.defaults({
-                headers: {
-                    'Authorization': 'Bearer ' + body.authToken
-                }
-            });
-            winston.log('info', 'AuthToken: %s', body.authToken);
-            return callback(null, authRequest);
-        }
-    });
+var actrMod = require('../actor');
+var urls = {
+    catalog : '/fermat/rest/api/v1/network/catalog',
+    data : '/fermat/rest/api/v1/network/data',
+    clients : '/fermat/rest/api/v1/network/clients',
+    actors : '/fermat/rest/api/v1/network/actors'
 };
-/**
- * [doRequest description]
- *
- * @method doRequest
- *
- * @param  {[type]}   auth     [description]
- * @param  {[type]}   options  [description]
- * @param  {[type]}   type     [description]
- * @param  {Function} callback [description]
- *
- * @return {[type]}   [description]
- */
-var doRequest = function(auth, options, type, callback) {
-    auth(options, function(err, res, bod) {
-        if (err) return callback(err, null);
-        if (bod) {
-            var data, body;
-            switch (type) {
-                case 0:
-                    data = JSON.parse(bod);
-                    data.registeredNetworkServiceDetail = JSON.parse(data.registeredNetworkServiceDetail);
-                    data.registerOtherComponentDetail = JSON.parse(data.registerOtherComponentDetail);
-                    break;
-                case 1:
-                    body = JSON.parse(bod);
-                    data = JSON.parse(body.data);
-                    break;
-                case 2:
-                    body = JSON.parse(bod);
-                    data = JSON.parse(body.rl);
-                    break;
-                case 3:
-                    body = JSON.parse(bod);
-                    data = JSON.parse(body.rl);
-                    break;
+var port = '8080';
+
+var doRequest = function(ip, route, callback) {
+    var url = buildURL(ip, route);
+    var data = {};
+    
+    request({
+        url : url,
+        json : true,
+        method: 'GET'
+    }, function(error, response, body) {
+        if(error) {
+            return callback(error, null);
+        }
+        
+        if(body && response.statusCode === 200) {
+            
+            data = parseRecursively(body);
+            
+            if(data.error) {
+                winston.log('error', 'Fermat Node request failed with code %s: %s', data.error.code, data.error.message);
+                return callback(data.error, null);
             }
-            return callback(null, data);
         }
+        return callback(null, data);
     });
 };
+
 /**
- * [saveNetworkStatus description]
- *
- * @method saveNetworkStatus
- *
- * @param  {Function}        callback [description]
- *
- * @return {[type]}          [description]
+ * Parses a json object recursively
+ * @author Miguelcldn
+ * @param   {st}     input The input string
+ * @returns {object} A recursively-parsed object from the JSON
+ */
+var parseRecursively = function(input) {
+    var output = {};
+    try {
+        //Sanitize strings
+        // did't work output = input.split('\"').join('\\"');
+        //output = JSON.parse(input);
+        if(typeof input === 'string') output = JSON.parse(input);
+        else output = input;
+        
+        if(typeof output === 'object') {
+            for(var prop in output) {
+                var elm = output[prop];
+                if(typeof elm === 'string' && (elm[0] === '{' || elm[0] === '[')) {
+                    output[prop] = parseRecursively(elm);
+                }
+            }
+        }
+    } catch(e) {
+        output = {};
+        winston.log('error', 'Could not parse the input %s', input);
+        //winston.log('error', '%j', e);
+    }
+    return output;
+};
+
+/**
+ * Builds the URL to interact with the Fermat Node's API
+ * @author Miguelcldn
+ * @param   {string} ip    The ip con use as root
+ * @param   {string} route The type of route to obtain
+ * @returns {string} A full URL for the request
+ */
+var buildURL = function(ip, route) {
+    return 'http://' + ip + ':' + port + urls[route];
+};
+
+/**
+ * Extract the service data from a server
+ * @author Miguelcldn
+ * @param   {string} ip     Ip of the server
+ */
+var extractServerData = function(ip, callback) {
+    doRequest(ip, 'data', function(err, data) {
+        if(err) return callback(err, null);
+        servMod.insertServer(data.hash, data.location, ip, data.networkServices, callback);
+    });
+};
+
+/**
+ * Extract the clients from a server
+ * @author Miguelcldn
+ * @param   {object}   serverModel Server object
+ * @param   {string}   ip          IP of the server
+ */
+var extractServerClients = function(serverModel, ip, callback) {
+    var printResult = function(err, data) {
+        if(err) {
+            winston.log('error', 'Could not save clients model. %j', err, {});
+        }
+    };
+    
+    doRequest(ip, 'clients', function(err, data) {
+        if(err) return callback(err, null);
+        
+        servMod.updateServer(serverModel._id, serverModel._wave_id, {conectedClients : data.length}, function(err, data) {
+            if(err) winston.log('error', 'Could not update server clients, ip: %s', ip, {});
+        });
+        
+        for(var i = 0; i < data.length; i++) {
+            
+            var client = data[i];
+        
+            clintMod.insertClient(serverModel._wave_id, serverModel._id, client.hash, client.location, client.networkServices, printResult);
+        }
+        
+        return callback(null, data);
+    });
+};
+
+/**
+ * Extract the actors from a server
+ * @author Miguelcldn
+ * @param   {object}   serverModel Server object
+ * @param   {string}   ip          IP of the server
+ */
+var extractServerActors = function(serverModel, ip, callback) {
+    var printResult = function(err, data) {
+        if(err) {
+            winston.log('error', 'Could not save actors model. %j', err, {});
+        }
+    };
+    
+    doRequest(ip, 'actors', function(err, data) {
+        if(err) return callback(err, null);
+        
+        for(var i = 0; i < data.length; i++) {
+            
+            var actor = data[i];
+            
+            actrMod.insertActor(serverModel._wave_id, serverModel._id, actor.hash, actor.type, actor.links, actor.location, actor.profile, printResult);
+        }
+        
+        return callback(null, data);
+    });
+};
+
+/**
+ * Extracts data, clients and actors from a server
+ * @author Miguelcldn
+ * @param {string}   nodeIP   IP of the server
+ */
+var extractAllData = function(nodeIP, callback) {
+    //Extract server data
+    extractServerData(nodeIP, function(err, server) {
+        if(err) {
+            winston.log('error', 'Could not get the server data from %s', nodeIP);
+            callback(err, null);
+            return;
+        }
+        //Extract clients from server
+        extractServerClients(server, nodeIP, function(err, client) {
+            if(err) {
+                winston.log('error', 'Could not get the server clients from %s', nodeIP);
+            }
+            //Extract actors from server
+            extractServerActors(server, nodeIP, function(err, actor) {
+                if(err) {
+                    winston.log('error', 'Could not get the server actors from %s', nodeIP);
+                }
+                
+                callback(null, server);
+            });
+        });
+    });
+};
+
+/**
+ * Saves a new wave of data
+ * @author Miguelcldn
  */
 exports.saveNetworkStatus = function(callback) {
-    var hash = SHA256(config.network.ip) + '';
-    var extra = {};
-    var reportResult = function(error, client) {
-        if (error) winston.log('error', 'Error on crawler', error);
-        if (client) {
-            winston.log('info', 'Client added!');
+    var seed = config.network.ip;
+    var nodeList = [seed];
+    var listLength = 1;
+    
+    var reportEnd = function(err, server) {
+        listLength--;
+        winston.log('info', 'Visited server %s with IP: %s', server.hash, server.lastIP);
+        if(listLength === 0) return callback(null, seed);
+    };
+    
+    doRequest(seed, 'catalog', function(err, resp) {
+        if(err) return callback(err, null);
+        
+        var list = resp.nodes;
+        
+        for(var i in list) {
+            var ip = list[i];
+            nodeList.push(ip);
+            listLength++;
         }
-    };
-    var fillClientData = function(auth, client, server, callback) {
-        doRequest(auth, {
-            url: 'http://' + config.network.ip + ':9090/fermat/api/admin/monitoring/client/components/details',
-            method: 'GET',
-            qs : {
-                i : client.identityPublicKey
-            }
-        }, 3, function(error, data) {
+        
+        for(i = 0; i < list.length; i++) {
+            var nodeIP = list.shift();
             
-            if(error) return callback(error, null);
-            
-            client.comps = data;
-            clintMod.insertClient(server._wave_id, server._id, client.identityPublicKey, client, callback);
-        });
-    };
-    doLogin(function(err, auth) {
-        doRequest(auth, {
-            url: 'http://' + config.network.ip + ':9090/fermat/api/admin/monitoring/current/data',
-            method: 'GET'
-        }, 0, function(error, current) {
-            if (error) return callback(error, null);
-            if (current) {
-                extra.current = current;
-                doRequest(auth, {
-                    url: 'http://' + config.network.ip + ':9090/fermat/api/admin/monitoring/system/data',
-                    method: 'GET'
-                }, 1, function(error, system) {
-                    if (error) return callback(error, null);
-                    if (system) {
-                        extra.system = system;
-                        satelize.satelize({
-                            ip: config.network.ip
-                        }, function(error, location) {
-                            if (error) return callback(error, null);
-                            if (location) {
-                                extra.location = location;
-                                servMod.insertServer(hash, extra, function(error, server) {
-                                    if (error) return callback(error, null);
-                                    if (server) {
-                                        callback(null, server);
-                                        doRequest(auth, {
-                                            url: 'http://' + config.network.ip + ':9090/fermat/api/admin/monitoring/clients/list',
-                                            method: 'GET'
-                                        }, 2, function(error, clients) {
-                                            if (error) winston.log('error', 'Error on crawler', error);
-                                            if (clients) {
-                                                for (var i = clients.length - 1; i >= 0; i--) {
-                                                    fillClientData(auth, clients[i], server, reportResult);
-                                                }
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        });
+            extractAllData(nodeIP, reportEnd);
+        }
     });
 };
