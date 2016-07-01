4344528628bd195b3e69fd9f612e884bab8d2e63
Miguel Celedon <miguelceledon@outlook.com>
2016-07-01 19:22:32 -0400

    Updated image-loader and images.json

--- api-server/image-loader.js
+++ api-server/image-loader.js
@@ -1,12 +1,9 @@
 var fs = require('fs');
-var ncp = require('ncp').ncp;
 var path = require('path');
 var winston = require('winston');
 var filename;
 var images = [];
-var root = '../platform-visualization/images';
-var destination = '../platform-visualization/images/wallet_screenshots';
-var source = '../../../Test/api-server/cache/production/fermat/seed-resources/wallet_screenshots';
+var root = '../clair/images';
 /**
  * [walkDir description]
  *
@@ -29,7 +26,7 @@ var walkDir = function (directoryName) {
                 walkFile(++i);
             } else {
                 if (fullPath.indexOf(".jpg") > -1 || fullPath.indexOf(".jpeg") > -1 || fullPath.indexOf(".png") > -1 || fullPath.indexOf(".gif") > -1) {
-                    var strFile = fullPath.replace('../platform-visualization', '.');
+                    var strFile = fullPath.replace('../clair', '.');
                     images.push(strFile);
                 }
                 walkFile(++i);
@@ -48,7 +45,7 @@ var walkDir = function (directoryName) {
 var loadImages = function () {
     walkDir(root);
     var imagesStr = JSON.stringify(images.sort());
-    fs.writeFile('../platform-visualization/json/images.json', imagesStr, {
+    fs.writeFile('../clair/json/images.json', imagesStr, {
         flags: 'w'
     }, function (err) {
         'use strict';
@@ -57,31 +54,5 @@ var loadImages = function () {
         }
     });
 };
-/**
- * copy the images from the screenshots
- *
- * @method
- *
- * @param  {string} source
- * @param  {string} destination
- * @param  {function} callback
- * 
- */
-try {
-    ncp(source, destination, function (err) {
-        if (err) {
-            winston.log('info', err.message, err);
-            loadImages();
-        } else {
-            source = '../../../Test/api-server/cache/development/fermat/seed-resources/wallet_screenshots';
-            ncp(source, destination, function (err) {
-                if (err) {
-                    winston.log('info', err.message, err);
-                }
-                loadImages();
-            });
-        }
-    });
-} catch (err) {
-    winston.log('info', err.message, err);
-}
+
+loadImages();
