664ff15cf76a7f9094ed08b3eddd8d50ab58a9a8
Luis Campo <campusprize@gmail.com>
2016-07-30 11:45:55 -0500

    Revert "#699 create function to get layers by super layers"
    This reverts commit 1669e5f854f4579621ec549a11075af283b144dd.

--- api-server/test/module/repository/layer/lay.js
+++
@@ -1,9 +0,0 @@
-var repMod = require('../../../../modules/repository/layer');
-var mongoose = require('mongoose');
-console.log('info', 'Starting database connexion');
-var db = require('../../../../db');
-console.log("Get layers by super layers");
-repMod.getLayersBySuprLays('ADD', function (err, res) {
-	if (err) return 'error';
-	if (res) console.log(res);
-});
