90a190decf35d4bf4855c7119aff00483f77d1a9
Miguel Celedon <miguelceledon@outlook.com>
2016-08-04 13:05:40 -0400

    Workflow setup

--- clair/gulpfile.js
+++ clair/gulpfile.js
@@ -1,17 +1,31 @@
+//*****************************************
+//*THIS FILE MIGHT BE NOT NECESARY ANYMORE*
+//*****************************************
+
+
 // Include gulp
 var gulp = require('gulp');
 // Include plugins
 var concat = require('gulp-concat');
+var tsconfig = require('gulp-tsconfig-update');
+var exec = require('gulp-exec');
+
 // Concatenate JS Files
 gulp.task('build', function() {
-    return gulp.src('src/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('.'));
+    gulp.src('.')
+    .pipe(exec('tsc'))
+    .pipe(exec.reporter());
+});
+
+gulp.task('build-tsconfig', function() {
+    gulp.src(['src/*.ts', 'src/*.d.js', 'src/*.js', '../typings/index.d.ts'])
+    .pipe(tsconfig());
 });
 
 // Default task
 gulp.task('default', ['build', 'watch']);
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.js', ['build']);
+    gulp.watch(['src/*.ts', 'src/*.js'], ['build']);
+    gulp.watch('tsconfig.json', ['build-tsconfig']);
 });
