da6b329f33de3ea5794d9579b8c85d3dfa73c097
Miguel Celedon <miguelceledon@outlook.com>
2016-08-10 15:04:18 -0400

    Updated loader

--- clair/src/loader.ts
+++ clair/src/loader.ts
@@ -1,11 +1,11 @@
-function Loader() {
+class Loader {
 
     /**
      * [getStamp description]
      * @method getStamp
      * @return {[type]} [description]
      */
-    function getStamp() {
+    getStamp() {
         var img = document.createElement("img");
         img.className = 'stamp';
         img.src = 'images/alt_not_found.png';
@@ -23,11 +23,11 @@ function Loader() {
      * check all elements in table
      * @method findThemAll
      */
-    this.findThemAll = function() {
-        for(var i = 0, l = table.length; i < l; i++) {
-            if(!table[i].found && table[i].code_level == "concept") {
+    findThemAll() {
+        for (var i = 0, l = table.length; i < l; i++) {
+            if (!table[i].found && table[i].code_level == "concept") {
                 var strIndex = "#" + i;
-                $(strIndex).append(getStamp());
+                $(strIndex).append(this.getStamp());
             }
         }
     };
