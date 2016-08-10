59c1e0cb0d0e7e426167b80a52b0b0b8d00d6e0e
Miguel Celedon <miguelceledon@outlook.com>
2016-08-10 15:03:52 -0400

    Updated map

--- clair/src/map.ts
+++ clair/src/map.ts
@@ -1,14 +1,14 @@
-var map = {};
+let map = {};
 
 /**
  * @author Miguel Celedon
  * Loads the map (json version)
  * @param {Function} callback Function to call when finished
  */
-function loadMap(callback){
-    
-    $.get("json/config_map.json", {}, function(json) {
-        window.map = json;
+function loadMap(callback) {
+
+    $.get("json/config_map.json", {}, function (json) {
+        map = json;
         callback();
     });
 }
